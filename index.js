acb74cc1b2e74bc11acefba66f3eb267f4836625
B1ue6ud <50664442+B1ue6ud@users.noreply.github.com>
2020-05-26 19:15:12 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 8080;
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname+"/index.html");
+});
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+	socket.on('disconnect', ()=>{
+		console.log('user disconnected');
+	});
+});
+
+io.on('connection', function(socket){
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+		console.log('message: '+msg);
+	});
+});
+
+http.listen(8080,()=>{
+	console.log('listening on 8080');
+});
